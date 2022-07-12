6958c4e59fd1c67c93f34d3067182383b3b060b8
dsampath <dsampath@wiley.com>
2022-07-12 17:08:54 +0530

    called a url to render a page when btn click

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,27 @@
 import {Route, useParams} from "react-router-dom";
 import {Fragment} from "react";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+    {id:'q1', author:'Max', text:'Learning React is Fun!'},
+    {id:'q2', author:'John', text:'Learning Java is Fun!'},
+    {id:'q3', author:'Peter', text:'Learning Python is Fun!'}
+];
 
 const QuoteDetail = () => {
     const params = useParams();
+
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+    if(!quote){
+        return <p>No quote found!!</p>
+    }
     return (
         <Fragment>
-            <h1>Quote Detail Page</h1>
-            <p>{params.quoteId}</p>
+            <HighlightedQuote text={quote.text} author={quote.author}/>
+            {/*<h1>Quote Detail Page</h1>*/}
+            {/*<p>{params.quoteId}</p>*/}
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 {/*now Comments component will load if the url is http:localhost:3000/quotes/123/comments */}
                 <Comments/>
